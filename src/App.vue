ec6bf9f9235b28d1b02b0b1358a90dde375a8fce
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-13 17:43:21 +0200

    add section top

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ export default {
 
 
 <template>
-  <header>
-    <div class="container-md d-flex align-items-center">
+  <header id="app_header">
+    <div class="container-md d-flex align-items-center justify-content-between">
       <div class="logo">
         <img src="./assets/img/dc-logo.png" alt="">
       </div>
@@ -25,6 +25,7 @@ export default {
       </nav>
     </div>
   </header>
+
   <main id="app_main">
     <div class="container-md align-items-center">
       <p>CONTENT GOES HERE</p>
@@ -37,14 +38,51 @@ export default {
     <section class="top_footer">
       <div class=" container-md m-auto row">
         <div class="col">
-          <img class=" d-inline-block" src="./assets/img/buy-comics-digital-comics.png" alt="">
-          <span>DIGITAL COMICS</span>
+          <a class=" text-decoration-none" href="">
+            <img src="./assets/img/buy-comics-digital-comics.png" alt="">
+            <span class="text-white ps-3">DIGITAL COMICS</span>
+          </a>
+        </div>
+        <div class="col">
+          <a class=" text-decoration-none" href="">
+            <img src="./assets/img/buy-comics-merchandise.png" alt="">
+            <span class=" text-white ps-3">DC MERCHANDISE</span>
+          </a>
+        </div>
+        <div class="col">
+          <a class=" text-decoration-none" href="">
+            <img src="./assets/img/buy-comics-subscriptions.png" alt="">
+            <span class=" text-white ps-3">SUBSCRIPTIONS</span>
+          </a>
+        </div>
+        <div class="col">
+          <a class=" text-decoration-none" href="">
+            <img src="./assets/img/buy-comics-shop-locator.png" alt="">
+            <span class=" text-white ps-3">COMIC SHOP LOCATOR</span>
+          </a>
+        </div>
+        <div class="col">
+          <a class=" text-decoration-none" href="">
+            <img src="./assets/img/buy-dc-power-visa.svg" alt="">
+            <span class=" text-white ps-3">DC POWER VISA</span>
+          </a>
         </div>
 
       </div>
     </section>
 
-    <section class=" main_footer"></section>
+    <section class="main_footer">
+      <div class="row">
+        <div class="col">
+          <h2>DC </h2>
+          <ul>
+            li
+          </ul>
+        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+      </div>
+    </section>
     <section class="bottom_footer"></section>
 
 
@@ -57,7 +95,7 @@ export default {
 <style lang="scss" scoped>
 // @use './node_modules/bootstrap/scss/bootstrap.scss';
 
-header {
+#app_header {
   text-align: center;
   background-color: #ffffff;
 
@@ -96,5 +134,24 @@ header {
 
 }
 
-#app_footer {}
+#app_footer {
+
+  .col {
+    margin: 2rem 0;
+    font-size: 0.7rem;
+    display: flex;
+    align-items: center;
+
+    a {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  .top_footer {
+    img {
+      max-width: 50px;
+    }
+  }
+}
 </style>

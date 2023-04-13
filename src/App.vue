139b69a1b9db8fa5b76e775bf9b91109a6e5c425
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-13 15:35:02 +0200

    add navbar template

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,39 @@ export default {
 
 <template>
   <header>
-    <h1>Comics</h1>
+    <div class="logo">
+      <img src="./assets/img/dc-logo.png" alt="">
+    </div>
+    <nav class="nav justify-content-center  ">
+      <a class="nav-link active" href="#" aria-current="page">CHARACTERS</a>
+      <a class="nav-link " href="#">COMICS</a>
+      <a class="nav-link " href="#">MOVIES</a>
+      <a class="nav-link " href="#">TV</a>
+      <a class="nav-link " href="#">GAMES</a>
+      <a class="nav-link " href="#">COLLECTIBLES</a>
+      <a class="nav-link " href="#">VIDEOS</a>
+      <a class="nav-link " href="#">FANS</a>
+      <a class="nav-link " href="#">NEWS</a>
+      <a class="nav-link " href="#">SHOP</a>
+    </nav>
   </header>
   <main></main>
   <footer></footer>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+  text-align: center;
+  background-color: #ffffff;
+
+  img {
+    padding: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+  }
+}
+</style>

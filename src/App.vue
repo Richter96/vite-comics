58bc05dae60dc5e8ed538546cab1d4c54c1a2865
Riccardo castiglione <castiglione96.rc@gmail.com>
2023-04-14 10:43:42 +0200

    create component siteHeader.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,16 @@
 <script>
+import siteHeader from './components/siteHeader.vue',
 export default {
+  data() {
+    return {
 
+    }
+  },
 }
 </script>
 
 
 <template>
-  <header id="app_header">
-    <div class="container-md d-flex align-items-center justify-content-between">
-      <div class="logo">
-        <img src="./assets/img/dc-logo.png" alt="">
-      </div>
-      <nav class="nav justify-content-center">
-        <a class="nav-link active" href="#" aria-current="page">CHARACTERS</a>
-        <a class="nav-link " href="#">COMICS</a>
-        <a class="nav-link " href="#">MOVIES</a>
-        <a class="nav-link " href="#">TV</a>
-        <a class="nav-link " href="#">GAMES</a>
-        <a class="nav-link " href="#">COLLECTIBLES</a>
-        <a class="nav-link " href="#">VIDEOS</a>
-        <a class="nav-link " href="#">FANS</a>
-        <a class="nav-link " href="#">NEWS</a>
-        <a class="nav-link " href="#">SHOP</a>
-      </nav>
-    </div>
-  </header>
-
   <main id="app_main">
     <div class="container-md align-items-center">
       <p>CONTENT GOES HERE</p>
@@ -33,45 +18,45 @@ export default {
   </main>
 
 
+  <section class="banner">
+    <div class=" container-md m-auto row">
+      <div class="col">
+        <a class=" text-decoration-none" href="">
+          <img src="./assets/img/buy-comics-digital-comics.png" alt="">
+          <span class="text-white ps-3">DIGITAL COMICS</span>
+        </a>
+      </div>
+      <div class="col">
+        <a class=" text-decoration-none" href="">
+          <img src="./assets/img/buy-comics-merchandise.png" alt="">
+          <span class=" text-white ps-3">DC MERCHANDISE</span>
+        </a>
+      </div>
+      <div class="col">
+        <a class=" text-decoration-none" href="">
+          <img src="./assets/img/buy-comics-subscriptions.png" alt="">
+          <span class=" text-white ps-3">SUBSCRIPTIONS</span>
+        </a>
+      </div>
+      <div class="col">
+        <a class=" text-decoration-none" href="">
+          <img src="./assets/img/buy-comics-shop-locator.png" alt="">
+          <span class=" text-white ps-3">COMIC SHOP LOCATOR</span>
+        </a>
+      </div>
+      <div class="col">
+        <a class=" text-decoration-none" href="">
+          <img src="./assets/img/buy-dc-power-visa.svg" alt="">
+          <span class=" text-white ps-3">DC POWER VISA</span>
+        </a>
+      </div>
+
+    </div>
+  </section>
 
   <footer id="app_footer">
+
     <section class="top_footer">
-      <div class=" container-md m-auto row">
-        <div class="col">
-          <a class=" text-decoration-none" href="">
-            <img src="./assets/img/buy-comics-digital-comics.png" alt="">
-            <span class="text-white ps-3">DIGITAL COMICS</span>
-          </a>
-        </div>
-        <div class="col">
-          <a class=" text-decoration-none" href="">
-            <img src="./assets/img/buy-comics-merchandise.png" alt="">
-            <span class=" text-white ps-3">DC MERCHANDISE</span>
-          </a>
-        </div>
-        <div class="col">
-          <a class=" text-decoration-none" href="">
-            <img src="./assets/img/buy-comics-subscriptions.png" alt="">
-            <span class=" text-white ps-3">SUBSCRIPTIONS</span>
-          </a>
-        </div>
-        <div class="col">
-          <a class=" text-decoration-none" href="">
-            <img src="./assets/img/buy-comics-shop-locator.png" alt="">
-            <span class=" text-white ps-3">COMIC SHOP LOCATOR</span>
-          </a>
-        </div>
-        <div class="col">
-          <a class=" text-decoration-none" href="">
-            <img src="./assets/img/buy-dc-power-visa.svg" alt="">
-            <span class=" text-white ps-3">DC POWER VISA</span>
-          </a>
-        </div>
-
-      </div>
-    </section>
-
-    <section class="main_footer">
       <div class="row row-cols-6 container-md m-auto">
         <div class="col d-flex flex-column">
           <h5>DC COMICS</h5>
@@ -186,30 +171,30 @@ export default {
 
 }
 
-#app_footer {
-
-
-  .top_footer {
-    img {
-      max-width: 50px;
-    }
-
-    .col {
-      margin: 2rem 0;
-      font-size: 0.7rem;
-      display: flex;
-      align-items: center;
-
-      a {
-        display: flex;
-        align-items: center;
-        width: 100%;
-
-      }
-    }
+.banner {
+  img {
+    max-width: 50px;
   }
 
-  .main_footer {
+  .col {
+    margin: 2rem 0;
+    font-size: 0.7rem;
+    display: flex;
+    align-items: center;
+
+    a {
+      display: flex;
+      align-items: center;
+      width: 100%;
+
+    }
+  }
+}
+
+
+#app_footer {
+
+  .top_footer {
     background-image: url(./assets/img/footer-bg.jpg);
 
     ul {
---
+++ src/components/siteHeader.vue
@@ -0,0 +1,33 @@
+
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+}
+</script>
+
+<template>
+    <header id="app_header">
+        <div class="container-md d-flex align-items-center justify-content-between">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="">
+            </div>
+            <nav class="nav justify-content-center">
+                <a class="nav-link active" href="#" aria-current="page">CHARACTERS</a>
+                <a class="nav-link " href="#">COMICS</a>
+                <a class="nav-link " href="#">MOVIES</a>
+                <a class="nav-link " href="#">TV</a>
+                <a class="nav-link " href="#">GAMES</a>
+                <a class="nav-link " href="#">COLLECTIBLES</a>
+                <a class="nav-link " href="#">VIDEOS</a>
+                <a class="nav-link " href="#">FANS</a>
+                <a class="nav-link " href="#">NEWS</a>
+                <a class="nav-link " href="#">SHOP</a>
+            </nav>
+        </div>
+    </header>
+</template>
